070c34b482dc12a1b769003e3096bef3357145ca
Am1r-B <69463011+Am1r-B@users.noreply.github.com>
2023-05-06 21:00:34 +0330

    Grab elements and console buttons > counter

---
+++ 02-counter/setup/app.js
@@ -0,0 +1,8 @@
+// Set initial count
+let count = 0;
+
+// Select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+console.log(btns);
