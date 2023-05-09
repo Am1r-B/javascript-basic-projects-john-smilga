f8e03fead3307a896f1275952b05414544d72f95
Am1r-B <69463011+Am1r-B@users.noreply.github.com>
2023-05-09 19:15:34 +0330

    Log different based on input > grocery bud

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -20,6 +20,15 @@ form.addEventListener("submit", addItem);
 function addItem(e) {
   e.preventDefault();
   const value = grocery.value;
+  const id = new Date().getTime().toString();
+
+  if (value !== "" && editFlag === false) {
+    console.log("add item to the list");
+  } else if (value !== "" && editFlag === true) {
+    console.log("editing");
+  } else {
+    console.log("empty value");
+  }
 }
 
 // ****** LOCAL STORAGE **********
