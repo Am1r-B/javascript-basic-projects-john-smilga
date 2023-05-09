1751dca29750baa767cc5407a7e0efb8b819d7dd
Am1r-B <69463011+Am1r-B@users.noreply.github.com>
2023-05-09 19:16:07 +0330

    Make code cleaner > grocery bud

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -22,9 +22,9 @@ function addItem(e) {
   const value = grocery.value;
   const id = new Date().getTime().toString();
 
-  if (value !== "" && editFlag === false) {
+  if (value && !editFlag) {
     console.log("add item to the list");
-  } else if (value !== "" && editFlag === true) {
+  } else if (value && editFlag) {
     console.log("editing");
   } else {
     console.log("empty value");
